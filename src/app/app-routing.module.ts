1025360a87c2391282244328e6de455819993182
Israrul1 <cu.17bcs1963@gmail.com>
2021-10-11 14:56:08 +0530

    my changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  { path: 'home-component', component: HomeComponent },
+
+  { path: 'about-component', component: AboutComponent },
+
+  { path: 'product-component', component: ProductComponent },
+
+  { path: 'contact-component', component: ContactComponent },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
