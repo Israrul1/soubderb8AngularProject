4656798ca4aa1d7c9e44762f28c1c351a2f7f3d8
Israrul1 <cu.17bcs1963@gmail.com>
2021-10-12 14:33:33 +0530

    my final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProductComponent } from './product/product.component';
 import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+import {MatMenuModule} from '@angular/material/menu'; ////
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,9 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
